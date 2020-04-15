904e783eb947e24ce4358943ebcac53f5ee7c62b
Alexey Yurchenko <alexes.dev@gmail.com>
2020-04-15 11:36:03 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Написать функцию, которая принимает на вход массив чисел и возвращает массив
+// соотношений этих чисел с точностью до 2 знаков после запятой
+// [3, 1] -> [0.75, 0.25]
+// (values: numbers[]) => numbers[]
+module.exports.getRatio = (values) => {
+  return values;
+};
+
+// Написать функцию, которая группирует элементы массива по значению из функции fn
+// Функция fn не обязательна, если ее нет, то группировка происходит по значению элемента
+// (items: Array<T>, fn: (value: T) => K): { [key: K]: Array<T> }
+module.exports.groupBy = (items, fn) => {
+  return {};
+};
+
+// Написать функцию, которая принимает dot-путь к значению и возвращает по нему значение из объекта
+// get('my.nested.value', obj) должно делать тоже самое, что и obj.my.nested.value (только без исключений)
+// (path: string, obj: any) => any
+module.exports.get = (path, obj) => {
+  return obj;
+};
+
+// Переписать функцию без async/await
+// (initialValue: T, run: (v: T) => T) => Promise<T>
+module.exports.process = async (initialValue, run) => {
+  return initialValue;
+};
---
+++ index.test.js
@@ -0,0 +1,50 @@
+const { getRatio, get, groupBy, process } = require('./index');
+
+test('getRatio', () => {
+  expect(getRatio([])).toEqual([]);
+  expect(getRatio([10])).toEqual([1]);
+  expect(getRatio([1, 1, 1, 1])).toEqual([0.25, 0.25, 0.25, 0.25]);
+  expect(getRatio([1, 1, 1])).toEqual([0.34, 0.33, 0.33]);
+  expect(getRatio([2, 1])).toEqual([0.67, 0.33]);
+  expect(getRatio([3, 1])).toEqual([0.75, 0.25]);
+});
+
+test('groupBy', () => {
+  expect(groupBy([1, 1, 2, 2])).toEqual({
+    '1': [1, 1],
+    '2': [2, 2],
+  });
+
+  expect(groupBy([0, 1, 2, 3, 4, 5], (v) => v % 2)).toEqual({
+    '0': [0, 2, 4],
+    '1': [1, 3, 5],
+  });
+});
+
+test('get', () => {
+  const data = {
+    id: 10,
+    author: {
+      name: 'Pushkin',
+    },
+    comments: [
+      {
+        text: 'Super!',
+      },
+    ],
+  };
+
+  expect(get('id', data)).toBe(10);
+  expect(get('title', data)).toBe(undefined);
+  expect(get('author.name', data)).toBe('Pushkin');
+  expect(get('comments.0.text', data)).toBe('Super!');
+});
+
+test('process', async () => {
+  expect(await process(3, (n) => n * 2)).toBe(12);
+  expect(
+    await process(3, () => {
+      throw new Error('failed');
+    })
+  ).toBe('failed');
+});
